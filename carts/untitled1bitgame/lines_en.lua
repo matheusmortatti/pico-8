c8db4bfbd05358ea3782547fca434b6c0fa882a6
Matheus Mortatti <matheusmortatti@gmail.com>
2021-10-11 14:20:03 -0700

    forgot to add portuguese and english versions

---
+++ carts/untitled1bitgame/lines_en.lua
@@ -0,0 +1,9 @@
+lines={
+    [128]={'no one in this\nvillage knows what\nlies ahead of this\ngate...'},
+    [129]={'it\'s been years\nsince i felt time\npass...','what happened?⧗ '},
+    [130]={'where\'s little\njoseph?','left home to buy\nmilk and never\ncame back...'},
+    [131]={'everyone says\nthat the gate key\nlives somewhere\nin this forest','but no one has\never tried\nto find it!'},
+    [132]={'in all these years\ni have never been to\nthe northwest...','but someday I\'ll\nexplore this world!'},
+    [133]={'duuuude, i just left\nto buy milk and\nthese monsters\nattacked me!!','whatever you do,\ndon\'t go west😐'},
+    [134]={'thank you for\nplaying this\ndemo!!','i really hope you\nenjoyed your time\nwith it ♥'}
+}
